4508f1d4804069265007de04a0fffc1e5feff949
matthewcotton <matthewneilcotton@gmail.com>
2021-01-08 17:34:04 +0000

    tweek paper elevation

--- src/NewCard.js
+++ src/NewCard.js
@@ -15,48 +15,10 @@ const useStyles = makeStyles((theme) => ({
 
 function NewCard(props) {
   const classes = useStyles();
-  // const days = [
-  //   "Sunday",
-  //   "Monday",
-  //   "Tuesday",
-  //   "Wednesday",
-  //   "Thursday",
-  //   "Friday",
-  //   "Saturday",
-  // ];
-  // const day = new Date(parseInt(props.dateTime) * 1000);
-  // const today = new Date(Date.now())
-  // let nameDay = days[day.getDay(day)];
-  // const todayName = days[today.getDay(today)];
-  // nameDay = nameDay === todayName ? "Today" : nameDay;
-  // const months = [
-  //   "January",
-  //   "Febuary",
-  //   "March",
-  //   "April",
-  //   "May",
-  //   "June",
-  //   "July",
-  //   "August",
-  //   "September",
-  //   "October",
-  //   "November",
-  //   "December",
-  // ];
-  // const month = months[day.getMonth(day)];
-  // const date = day.getDate(day);
-  // const icon = `http://openweathermap.org/img/wn/${props.icon}@2x.png`;
-  // const description =
-  //   props.detailedDesc[0].toUpperCase() +
-  //   props.detailedDesc.slice(1, props.detailedDesc.length);
-  // const maxTemp = Math.round(props.maxTemp) + "°C";
-  // const minTemp = Math.round(props.minTemp) + "°C";
-  // const windSpeed = Math.round(props.windSpeed * 10) / 10 + "m/s";
-  // const windDirRot = String("rotate(" + props.windDir + "deg)");
 
   return (
     <Grid item xs={11} sm={6} md={4} lg={3} xl={2}>
-      <Paper className={classes.paper}>
+      <Paper className={classes.paper} elevation={5}>
         <Grid container>
           <Grid
             item
